24f0feac98335bb20f3813aa08ba897f5a8f783a
David <92366533+DavidJMT@users.noreply.github.com>
2026-05-19 13:54:18 +0000

    Updated urls

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,7 +16,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet, api_root
+
+import os
+from django.http import JsonResponse
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
+
+def api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f'https://{codespace_name}-8000.app.github.dev/'
+    else:
+        base_url = request.build_absolute_uri('/')
+    return JsonResponse({
+        'users': base_url + 'api/users/',
+        'teams': base_url + 'api/teams/',
+        'activities': base_url + 'api/activities/',
+        'workouts': base_url + 'api/workouts/',
+        'leaderboard': base_url + 'api/leaderboard/',
+    })
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
